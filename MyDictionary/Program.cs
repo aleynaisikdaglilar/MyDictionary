bf2d8bc7b579f018af5a8efd6d92742e4d493449
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2021-02-06 17:18:22 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Ogrenci = new MyDictionary<int, string>();
+            Ogrenci.Add(134, "Tolga Demir");
+            Ogrenci.Add(158, "Ayşe Kara");
+            Ogrenci.Add(115, "Nazlı Coşkun");
+            Ogrenci.Add(174, "Kemal Çiftçi");
+            Ogrenci.List();
+        }
+    }
+}
